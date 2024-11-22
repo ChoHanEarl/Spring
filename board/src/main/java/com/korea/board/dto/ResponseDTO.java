1030fdf975c12a526ff3897a7cde646f7fb11caa
ChoHanEarl <159342404+ChoHanEarl@users.noreply.github.com>
2024-11-22 16:41:23 +0900

    게시판 만들기(백엔드)

--- board/src/main/java/com/korea/board/dto/ResponseDTO.java
+++ board/src/main/java/com/korea/board/dto/ResponseDTO.java
@@ -1,10 +1,14 @@
 package com.korea.board.dto;
 
+import java.util.List;
+
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class ResponseDTO<T> {
 	private String error;
-	private T data;
+	private List<T> data;
 }
 
--- board/src/main/java/com/korea/board/model/BoardEntity.java
+++ board/src/main/java/com/korea/board/model/BoardEntity.java
@@ -18,7 +18,7 @@ public class BoardEntity {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long id;
 	private String title;
 	private String author;
 	private String writingTime;
